e04cc47ddd194570ca04f6a8a5f3ca20dff17e01
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-10-24 17:01:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,89 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import csv
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+import re
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import pandas as pd
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+def validate_email(email):
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+  """Validate the email address"""
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+  pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+  return bool(re.match(pattern, email))
+
+
+
+def submit_form(description, email):
+
+  """Submit the form data to the CSV file"""
+
+  data = {'Description': [description], 'Email': [email]}
+
+  df = pd.DataFrame(data)
+
+  try:
+
+    existing_df = pd.read_csv('user_requests.csv')
+
+    combined_df = pd.concat([existing_df, df])
+
+    combined_df.to_csv('user_requests.csv', index=False)
+
+  except FileNotFoundError:
+
+    df.to_csv('user_requests.csv', index=False)
+
+
+
+def ask_me_a_question_form():
+
+  """The 'Ask me a question' form"""
+
+  st.subheader("Ask me a question:")
+
+  description = st.text_area(label="A description of why you're reaching out, including any questions you have")
+
+  email = st.text_input(label="What email can I reach you at?")
+
+
+
+  submitted = st.form_submit_button("Submit")
+
+
+
+  if submitted:
+
+    if not validate_email(email):
+
+      st.error("Invalid email address")
+
+    else:
+
+      submit_form(description, email)
+
+      st.success("Thank you for your submission!")
+
+
+
+def main():
+
+  st.title("My Small Business")
+
+  st.subheader("Description of my small business", divider="violet")
+
+
+
+  with st.form("Ask me a question:", clear_on_submit=True):
+
+    ask_me_a_question_form()
+
+
+
+if __name__ == "__main__":
+
+  main()
